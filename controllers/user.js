0a126a392ed70983c77dbab023372913aa4ab037
MuhammadArslanKhan96 <m.arslan2201@gmail.com>
2022-06-28 20:33:22 +0500

    swap added

--- controllers/user.js
+++ controllers/user.js
@@ -28,16 +28,7 @@ const get_users = async (req, res, next) => {
     // Object.entries(wallet).forEach((entry) => {
     //   console.log({entry});
     // });
-    const MP = new Token(
-      UNISWAP.ChainId.MUMBAI,
-      "0x6fFd66587d2aa6a629fAD7b00fD2847b7d79A989",
-      18
-  );
-
-    console.log(WETH[MP.chainId])
-    const resp = await swapTokens(MP, WETH[MP.chainId], 0.001)
-    console.log(resp)
-    res.json({message: "wallet"}); // dummy function for now
+    
 };
 
 const create_user =  async (req, res, next) => {
@@ -84,57 +75,3 @@ module.exports = {
 
 
 
-async function swapTokens(token1, token2, amount, slippage = "50") {
-
-
-  try {
-      const wallet = new ethers.Wallet("71290a342cc96d86dff7e4bb244895a7cca3fff4e9830e87f0411d2213e1ee41", provider)
-      const pair = await Fetcher.fetchPairData(token1, token2, provider); //creating instances of a pair
-      const route = await new Route([pair], token2); // a fully specified path from input token to output token
-      let amountIn = ethers.utils.parseEther(amount.toString()); //helper function to convert ETH to Wei
-      amountIn = amountIn.toString()
-      
-      const slippageTolerance = new Percent(slippage, "10000"); // 50 bips, or 0.50% - Slippage tolerance
-  
-      const trade = new Trade( //information necessary to create a swap transaction.
-              route,
-              new TokenAmount(token2, amountIn),
-              TradeType.EXACT_INPUT
-      );
-
-      const amountOutMin = trade.minimumAmountOut(slippageTolerance).raw; // needs to be converted to e.g. hex
-      const amountOutMinHex = ethers.BigNumber.from(amountOutMin.toString()).toHexString();
-      const path = [token2.address, token1.address]; //An array of token addresses
-      const to = wallet.address; // should be a checksummed recipient address
-      const deadline = Math.floor(Date.now() / 1000) + 60 * 20; // 20 minutes from the current Unix time
-      const value = trade.inputAmount.raw; // // needs to be converted to e.g. hex
-      const valueHex = await ethers.BigNumber.from(value.toString()).toHexString(); //convert to hex string
-  
-      //Return a copy of transactionRequest, The default implementation calls checkTransaction and resolves to if it is an ENS name, adds gasPrice, nonce, gasLimit and chainId based on the related operations on Signer.
-      const rawTxn = await ROUTER_CONTRACT.populateTransaction.swapExactETHForTokens(amountOutMinHex, path, to, deadline, {
-          value: valueHex
-      })
-  
-      //Returns a Promise which resolves to the transaction.
-      let sendTxn = (await wallet).sendTransaction(rawTxn)
-      
-
-      //Resolves to the TransactionReceipt once the transaction has been included in the chain for x confirms blocks.
-      let reciept = (await sendTxn).wait()
-
-      //Logs the information about the transaction it has been mined.
-      if (reciept) {
-          console.log(" - Transaction is mined - " + '\n' 
-          + "Transaction Hash:", (await sendTxn).hash
-          + '\n' + "Block Number: " 
-          + (await reciept).blockNumber + '\n' 
-          + "Navigate to https://rinkeby.etherscan.io/txn/" 
-          + (await sendTxn).hash, "to see your transaction")
-      } else {
-          console.log("Error submitting transaction")
-      }
-
-  } catch(e) {
-      console.log(e)
-  }
-}
--- routes/transaction.js
+++ routes/transaction.js
@@ -6,6 +6,7 @@ const transaction = require('../controllers/transaction');
 
 // Router
 router.get('/send_coin', transaction.send_coin); 
+router.get('/swapExactETHForTokens', transaction.swapExactETHForTokens); 
 // router.post('/create_user', transaction.create_user); 
 // router.put('/update_user', transaction.update_user); 
 module.exports = router; // export to use in server.js
